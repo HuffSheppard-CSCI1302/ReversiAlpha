f357c06c71f26c388a064c9a764731e105dcae84
Miles B Huff <Sweyn78@users.noreply.github.com>
2016-03-02 16:20:37 -0500

    Update Board.java
    Comments and stuff

--- src/Board.java
+++ src/Board.java
@@ -1,11 +1,16 @@
 public interface Board
 { //Interface
+
+  // Variables
+  private char[9][9] caaGrid ;
+  
+  // Methods
   public  boolean    canMove()           ;
-  private char[9][9] caaGrid             ;
   public  char[9][9] getGrid()           ;
   public  int[2]     getScore()          ;
   public  String     toString()          ;
   public  void       calcMoves()         ;
   public  void       endGame()           ;
   public  void       setCoord(int ia[2]) ;
+  
 } //Interface
