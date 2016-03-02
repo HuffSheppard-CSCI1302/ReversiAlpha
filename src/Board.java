7aa7d20f50596d2dc8ac258641e598e15b705142
Miles B Huff <Sweyn78@users.noreply.github.com>
2016-03-02 17:21:00 -0500

    Update Board.java
    cheaper tabulation

--- src/Board.java
+++ src/Board.java
@@ -5,12 +5,12 @@ public interface Board
   private char[9][9] caaGrid ;
   
   // Methods
-  public  boolean    canMove()                   ;
-  public  char[9][9] getGrid()                   ;
-  public  int[2]     getScore()                  ;
-  public  String     toString()                  ;
-  public  void       calcMoves()                 ;
-  public  void       endGame()                   ;
+  public  boolean    canMove()   ;
+  public  char[9][9] getGrid()   ;
+  public  int[2]     getScore()  ;
+  public  String     toString()  ;
+  public  void       calcMoves() ;
+  public  void       endGame()   ;
   public  void       setCoord(char c, int ia[2]) ;
   public  void       setCoord(char[9][9] caa)    ;
   
