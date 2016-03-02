a33f13789b93ca9e5accae18111406cc5b69b21e
Miles B Huff <Sweyn78@users.noreply.github.com>
2016-03-02 17:17:57 -0500

    Update Board.java
    simplification

--- src/Board.java
+++ src/Board.java
@@ -5,13 +5,13 @@ public interface Board
   private char[9][9] caaGrid ;
   
   // Methods
-  public  boolean    canMove()                        ;
-  public  char[9][9] getGrid()                        ;
-  public  int[2]     getScore()                       ;
-  public  String     toString()                       ;
-  public  void       calcMoves()                      ;
-  public  void       endGame()                        ;
-  public  void       setCoord(char cPiece, int ia[2]) ;
-  public  void       setCoord(char[9][9] caa)         ;
+  public  boolean    canMove()                   ;
+  public  char[9][9] getGrid()                   ;
+  public  int[2]     getScore()                  ;
+  public  String     toString()                  ;
+  public  void       calcMoves()                 ;
+  public  void       endGame()                   ;
+  public  void       setCoord(char c, int ia[2]) ;
+  public  void       setCoord(char[9][9] caa)    ;
   
 } //interface
