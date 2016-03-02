969b5e83e5bbeee4224815f0b0c164f4cd8d8b7b
Miles B Huff <Sweyn78@users.noreply.github.com>
2016-03-02 16:49:49 -0500

    Update ReversiBoard.java
    comments and stuff

--- src/ReversiBoard.java
+++ src/ReversiBoard.java
@@ -1,45 +1,45 @@
-public class ReversiBoard
+public class ReversiBoard implements Board
 { //class
   private char[9][9] caaGrid ;
   
   public ReversiBoard()
-  {
+  { //method
     //TODO
-  }
+  } //method
   
   public  boolean    canMove()
-  {
+  { //method
     //TODO
-  }
+  } //method
   
   public  char[9][9] getGrid()
-  {
+  { //method
     //TODO
-  }
+  } //method
   
   public  int[2]     getScore()
-  {
+  { //method
     //TODO
-  }
+  } //method
   
   public  String     toString()
-  {
+  { //method
     //TODO
-  }
+  } //method
   
   public  void       calcMoves()
-  {
+  { //method
     //TODO
-  }
+  } //method
   
   public  void       endGame()
-  {
+  { //method
     //TODO
-  }
+  } //method
   
   public  void       setCoord(int ia[2])
-  {
+  { //method
     //TODO
-  }
+  } //method
   
 } //class
