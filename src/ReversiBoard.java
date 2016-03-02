f66bc82f9554aaa4d5daedeb980a44d5823b90e6
Miles B Huff <Sweyn78@users.noreply.github.com>
2016-03-02 16:45:04 -0500

    Update ReversiBoard.java
    added a barebones skeleton.

--- src/ReversiBoard.java
+++ src/ReversiBoard.java
@@ -1,1 +1,45 @@
-
+public class ReversiBoard
+{ //class
+  private char[9][9] caaGrid ;
+  
+  public ReversiBoard()
+  {
+    //TODO
+  }
+  
+  public  boolean    canMove()
+  {
+    //TODO
+  }
+  
+  public  char[9][9] getGrid()
+  {
+    //TODO
+  }
+  
+  public  int[2]     getScore()
+  {
+    //TODO
+  }
+  
+  public  String     toString()
+  {
+    //TODO
+  }
+  
+  public  void       calcMoves()
+  {
+    //TODO
+  }
+  
+  public  void       endGame()
+  {
+    //TODO
+  }
+  
+  public  void       setCoord(int ia[2])
+  {
+    //TODO
+  }
+  
+} //class
