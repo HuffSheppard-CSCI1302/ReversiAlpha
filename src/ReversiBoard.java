3b682c1316452c48eb252076eef40d82c2bee554
Miles B Huff <Sweyn78@users.noreply.github.com>
2016-03-02 17:52:30 -0500

    Update ReversiBoard.java
    Started implementing stuff.

--- src/ReversiBoard.java
+++ src/ReversiBoard.java
@@ -1,43 +1,112 @@
 public class ReversiBoard implements Board
 { //class
   private char[9][9] caaGrid ;
+
+  /* KEY:  
+   * '.' = empty
+   * 'X' = Dark (player 1)
+   * 'O' = Light (player 2)
+   * '_' = Possible move
+   */
   
+  ////////////////////////////////////////////////////////////////////////////
+  /** Constructor-method.  Fills the grid with axis-numbers and blanks.  
+   * @author Miles B Huff
+  **/
   public ReversiBoard()
   { //method
-    //TODO
+    // x-axis
+    for(int i = 1; i < 9; i++)
+    { // 2+
+      caaGrid[0][i] = i;
+    } // 2-
+
+    // y-axis
+    for(int i = 1; i < 9; i++)
+    { // 2+
+      caaGrid[i][0] = i;
+    } // 2-
+
+    // center
+    for(int i = 1; i < 9; i++)
+    { // 2+
+      for(int j = 1; j < 9; j++)
+      { // 3+
+        caaGrid[i][j] = '.' ;
+      } // 3-
+    } // 2-
   } //method
   
-  public  boolean    canMove()
+  ////////////////////////////////////////////////////////////////////////////
+  /** Figures out if there are any moves left.  
+   * @author Miles B Huff
+   * @return true if there are moves left.  
+  **/
+  @Override
+  public boolean canMove()
   { //method
     //TODO
   } //method
   
-  public  char[9][9] getGrid()
+  @Override
+  public char[9][9] getGrid()
   { //method
     //TODO
   } //method
   
-  public  int[2]     getScore()
+  @Override
+  public int[2] getScore()
   { //method
     //TODO
   } //method
   
-  public  String     toString()
+  ////////////////////////////////////////////////////////////////////////////
+  /** Converts the grid into a String.  
+   * @author Miles B Huff
+  **/
+  @Override
+  public String toString()
+  { //method
+    String s = '\n' ;
+    for(int i = 0; i < 9; i++)
+    { // 2+
+      s+= ' ' ;
+      for(int j = 0; j < 9; j++)
+      { // 3+
+        s+= ' ' ;
+        s+= caaGrid[i][j]) ;
+      } // 3-
+    s+= '\n' ;
+    } // 2-
+    s+= '\n' ;
+  } //method
+  
+  ////////////////////////////////////////////////////////////////////////////
+  /** 
+   * @author Miles B Huff
+  **/
+  @Override
+  public void calcMoves()
   { //method
     //TODO
   } //method
   
-  public  void       calcMoves()
+  ////////////////////////////////////////////////////////////////////////////
+  /** 
+   * @author Miles B Huff
+  **/
+  @Override
+  public void endGame()
   { //method
     //TODO
   } //method
   
-  public  void       endGame()
-  { //method
-    //TODO
-  } //method
-  
-  public  void       setCoord(int ia[2])
+  ////////////////////////////////////////////////////////////////////////////
+  /** 
+   * @author Miles B Huff
+  **/
+  @Override
+  public void setCoord(int ia[2])
   { //method
     //TODO
   } //method
