42b8635c9c05da8a591b004db42ed76f6595ea9b
Miles B Huff <Sweyn78@users.noreply.github.com>
2016-03-02 16:59:45 -0500

    Update Board.java
    added a new method

--- src/Board.java
+++ src/Board.java
@@ -5,12 +5,13 @@ public interface Board
   private char[9][9] caaGrid ;
   
   // Methods
-  public  boolean    canMove()           ;
-  public  char[9][9] getGrid()           ;
-  public  int[2]     getScore()          ;
-  public  String     toString()          ;
-  public  void       calcMoves()         ;
-  public  void       endGame()           ;
-  public  void       setCoord(int ia[2]) ;
+  public  boolean    canMove()                        ;
+  public  char[9][9] getGrid()                        ;
+  public  int[2]     getScore()                       ;
+  public  String     toString()                       ;
+  public  void       calcMoves()                      ;
+  public  void       endGame()                        ;
+  public  void       setCoord(int ia[2], char cPiece) ;
+  public  void       setCoord(int caa[9][9])          ;
   
 } //interface
