2a1313cff31e7076244c15282af4d80a6a4883e9
Miles B Huff <Sweyn78@users.noreply.github.com>
2016-03-31 13:52:33 -0400

    Update Board.java
    spacing

--- src/Board.java
+++ src/Board.java
@@ -19,16 +19,16 @@
 
 public interface Board
 { //interface
-  
+
   // Variables
   public char[][] caaGrid = new char[9][9];
-  
+
   // Methods
-  public  boolean  canMove() ;
-  public  char[][] getGrid() ;
-  public  int[]    getScore();
-  public  void     calcMoves(char cPiece);
-  public  void     setCoord(char cPiece, int[] iaCoord);
-  public  void     setGrid(char[][] caaGrid);
-  
+  public boolean  canMove  ();
+  public char[][] getGrid  ();
+  public int []   getScore ();
+  public void     calcMoves(char cPiece);
+  public void     setCoord (char cPiece, int[] iaCoord);
+  public void     setGrid  (char[][] caaGrid);
+
 } //interface
